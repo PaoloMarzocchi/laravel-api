817b510a81390d62458b012a288e835633c97261
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-05-30 16:08:14 +0200

    add route in api.php and add  API/ProjectController with index method

---
+++ app/Http/Controllers/API/ProjectController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+
+        $projects = Project::with('type', 'technologies')->orderByDesc('id')->paginate(8);
+
+        return response()->json([
+            'success' => true,
+            'projects' => $projects
+        ]);
+    }
+}
