db96b7d4074916daca5ed42396e6378e8b4590f8
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-06-03 10:43:35 +0200

    add route for show in api.php, add show method in API/ProjectController

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -11,11 +11,28 @@ class ProjectController extends Controller
     public function index()
     {
 
-        $projects = Project::with('type', 'technologies')->orderByDesc('id')->paginate(8);
+        $projects = Project::with('type', 'technologies')->orderByDesc('id')->paginate(4);
 
         return response()->json([
             'success' => true,
             'projects' => $projects
         ]);
+    }
+
+    public function show($slug)
+    {
+        $project = Project::with('type', 'technologies')->where('slug', $slug)->first();
+
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'response' => $project
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'response' => '404 | Nothing found'
+            ]);
+        }
     }
 }
